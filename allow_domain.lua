1c7d3d8ffcfd4694fc975d606799521829cae160
semyon422 <semyon422@users.noreply.github.com>
2023-06-13 22:20:36 +0500

    Update

--- allow_domain.lua
+++ allow_domain.lua
@@ -1,10 +1,12 @@
 local _domains = {
 	"dfjk.ru",
 	"smc.dfjk.ru",
+	"10.dfjk.ru",
 	"soundsphere.xyz",
 	"www.soundsphere.xyz",
 	"download.soundsphere.xyz",
 	"dl.soundsphere.xyz",
+	"10key.soundsphere.xyz",
 }
 
 local domains = {}
