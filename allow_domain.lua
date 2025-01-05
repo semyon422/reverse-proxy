9ecf526c57f9cea93596790d0fd34f625b2e8410
semyon422 <semyon422@users.noreply.github.com>
2025-01-05 11:51:28 +0500

    Delete shanghaimoon.ru, update allow_domain.lua

--- allow_domain.lua
+++ allow_domain.lua
@@ -2,12 +2,21 @@ local _domains = {
 	"dfjk.ru",
 	"smc.dfjk.ru",
 	"10.dfjk.ru",
+
 	"soundsphere.xyz",
 	"www.soundsphere.xyz",
 	"download.soundsphere.xyz",
 	"dl.soundsphere.xyz",
 	"10key.soundsphere.xyz",
-	"shanghaimoon.ru",
+
+	"c.dfjk.ru",
+	"ce.dfjk.ru",
+	"c4.dfjk.ru",
+	"osu.dfjk.ru",
+	"b.dfjk.ru",
+	"api.dfjk.ru",
+	"assets.dfjk.ru",
+	"a.dfjk.ru",
 }
 
 local domains = {}
